bf907326a122aa82cbf930035f3521fff82b2804
Gopi Srungavarapu <Gopi.Srungavarapu@rotary.org>
2020-04-18 17:51:09 -0500

    added CSS project

--- projects/index.php
+++ projects/index.php
@@ -1,48 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link rel="shortcut icon" href="../images/favicon-32x32.png" type="image/x-icon">
     <title>Projects</title>
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-    />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
     <script src="https://kit.fontawesome.com/078c5ccadb.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Bangers|Press+Start+2P&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
+</head>
 
-    <h1 class="text-center display-3 text-light border-bottom border-danger pb-4 mt-3">🛠️ Projects Under Progress 🚧</h1>
+<body>
+
+    <h1 class="text-center display-3 text-light border-bottom border-danger pb-4 mt-3">🛠️ Projects Under Progress 🚧
+    </h1>
     <div class="container">
-      <div class="row mt-4">
-        <div class="col-md-6 mt-5">
-            <a href="https://bootstrap.gopibabu.live" target="_blank" class="btn btn-lg btn-info form-control font-weight-bold mb-3">BOOTSTRAP</a>
-            <a href="https://restapi.gopibabu.live" target="_blank" class="btn btn-lg btn-danger form-control font-weight-bold mb-3">REST API</a>
-            <a href="https://react.gopibabu.live" target="_blank" class="btn btn-lg btn-warning form-control font-weight-bold mb-3">REACT JS</a>
-            <a href="https://node.gopibabu.live" target="_blank" class="btn btn-lg btn-info form-control font-weight-bold mb-3">NODE JS</a>
-            <a href="https://drupal7.gopibabu.live" target="_blank" class="btn btn-lg btn-secondary form-control font-weight-bold mb-3">DRUPAL</a>
-            <a href="https://laravel.gopibabu.live" target="_blank" class="btn btn-lg btn-danger form-control font-weight-bold mb-3">LARAVEL</a>
-            <a href="https://php.gopibabu.live" target="_blank" class="btn btn-lg btn-success form-control font-weight-bold mb-3">PHP</a>
-            <a href="https://myflix.gopibabu.live" target="_blank" class="btn btn-lg btn-primary form-control font-weight-bold mb-3">MYFLIX</a>
+        <div class="row mt-4">
+            <div class="col-md-6 mt-5">
+                <a href="https://bootstrap.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-info form-control font-weight-bold mb-3">BOOTSTRAP</a>
+                <a href="https://restapi.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-danger form-control font-weight-bold mb-3">REST API</a>
+                <a href="https://react.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-warning form-control font-weight-bold mb-3">REACT JS</a>
+                <a href="https://node.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-info form-control font-weight-bold mb-3">NODE JS</a>
+                <a href="https://drupal7.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-secondary form-control font-weight-bold mb-3">DRUPAL</a>
+                <a href="https://laravel.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-danger form-control font-weight-bold mb-3">LARAVEL</a>
+                <a href="https://php.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-success form-control font-weight-bold mb-3">PHP</a>
+                <a href="https://myflix.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-primary form-control font-weight-bold mb-3">MYFLIX</a>
+                <a href="https://css.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-info form-control font-weight-bold mb-3">CSS</a>
+            </div>
+            <div class="col-md-6 mt-5">
+                <a href="https://portfolio.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-primary form-control font-weight-bold mb-3">PORTFOLIO</a>
+                <a href="https://blog.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-info form-control font-weight-bold mb-3">BLOG</a>
+                <a href="https://assets.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-secondary form-control font-weight-bold mb-3">ASSETS</a>
+                <a href="https://notes.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-warning form-control font-weight-bold mb-3">NOTES</a>
+                <a href="https://typing.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-danger form-control font-weight-bold mb-3">TYPING</a>
+                <a href="https://photos.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-success form-control font-weight-bold mb-3">PHOTOS</a>
+                <a href="https://ui.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-primary form-control font-weight-bold mb-3">HTML & CSS</a>
+                <a href="https://js.gopibabu.live" target="_blank"
+                    class="btn btn-lg btn-info form-control font-weight-bold mb-3">JAVASCRIPT</a>
+            </div>
         </div>
-        <div class="col-md-6 mt-5">
-            <a href="https://portfolio.gopibabu.live" target="_blank" class="btn btn-lg btn-primary form-control font-weight-bold mb-3">PORTFOLIO</a>
-            <a href="https://blog.gopibabu.live" target="_blank" class="btn btn-lg btn-info form-control font-weight-bold mb-3">BLOG</a>
-            <a href="https://assets.gopibabu.live" target="_blank" class="btn btn-lg btn-secondary form-control font-weight-bold mb-3">ASSETS</a>
-            <a href="https://notes.gopibabu.live" target="_blank" class="btn btn-lg btn-warning form-control font-weight-bold mb-3">NOTES</a>
-            <a href="https://typing.gopibabu.live" target="_blank" class="btn btn-lg btn-danger form-control font-weight-bold mb-3">TYPING</a>
-            <a href="https://photos.gopibabu.live" target="_blank" class="btn btn-lg btn-success form-control font-weight-bold mb-3">PHOTOS</a>
-            <a href="https://ui.gopibabu.live" target="_blank" class="btn btn-lg btn-primary form-control font-weight-bold mb-3">HTML & CSS</a>
-            <a href="https://js.gopibabu.live" target="_blank" class="btn btn-lg btn-info form-control font-weight-bold mb-3">JAVASCRIPT</a>
-        </div>
-    </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  </body>
+</body>
+
 </html>
